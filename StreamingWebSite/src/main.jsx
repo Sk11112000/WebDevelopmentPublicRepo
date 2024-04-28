500ed7252371fd797e95a99398463e820fd64570
Sk11112000 <sunnykulshrestha1111@gmail.com>
2024-04-28 12:45:44 +0530

    added the loginAndSignup page structure

---
+++ StreamingWebSite/src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import LoginAndSignUp from "./LoginAndSignUp/LoginAndSignUp";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <LoginAndSignUp />
+  </React.StrictMode>
+);
